c9bb4fa907517e2bc08400c0389a7b73fef1a1b0
Sentinel-1 <sentinel-1@users.noreply.github.com>
2022-06-27 08:18:17 +0400

    Add update routine for the custom 404 page

--- update_docs.sh
+++ update_docs.sh
@@ -29,3 +29,21 @@ DOCS_DIR="${SCRIPT_DIR}/docs"
 "${ENV_BIN}jupyter-nbconvert" "${NOTEBOOK_NAME}" \
   --embed-images --to pdf --output-dir="${DOCS_DIR}"
 
+##
+# Update custom 404 page
+##
+UPDATED_404_TODAY=$(python3 <<EOF
+import os
+from datetime import datetime as dt
+from datetime import timedelta as td
+print((dt.now() - dt.fromtimestamp(os.path.getctime("docs/404.html"))) < td(days=1))
+EOF
+)
+# Rate limiting to one download per day:
+if [ "${UPDATED_404_TODAY}" != "True" ];then
+    echo "Updating custom 404 page"
+    rm -f "${DOCS_DIR}/404.html"
+    wget --output-document="${DOCS_DIR}/404.html" "https://raw.githubusercontent.com/sentinel-1/sentinel-1.github.io/master/docs/404.html"
+else
+    echo "SKIPPING custom 404 page update"
+fi
